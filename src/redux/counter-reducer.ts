174fbadb432327c11f4202a0dba7bda0b20ee658
Ekaterina <kattekly@mail.ru>
2023-01-26 09:51:16 +0300

    With Redux

--- src/redux/counter-reducer.ts
+++ src/redux/counter-reducer.ts
@@ -8,7 +8,7 @@ const initialState: initialStateType = {
 }
 
 
-export const counterReducer = (state: initialStateType = initialState, action: counterType) => {
+export const counterReducer = (state: initialStateType = initialState, action: counterType): initialStateType => {
     switch (action.type) {
         case "SET_COUNTER_VALUE": {
             return {
@@ -24,7 +24,7 @@ export const setCounterValueAC = (value: number) => {
     return {
         type: SET_COUNTER_VALUE,
         payload: value
-    }
+    } as const
 }
 
 type counterType = ReturnType<typeof setCounterValueAC>
